04b9511a52c255cea98079df726a96d52c774d09
Aleksander Mikucki <mikucki@gmail.com>
2017-06-16 01:24:28 +0200

    Moved both file to a common folder, a new cleaner solution for task2

--- task2/CCFrontTask2.js
+++ task2/CCFrontTask2.js
@@ -1,11 +1,35 @@
 /**
  * Created by Aleksander Mikucki on 15.06.2017.
+ *
+ * The way this code works:
+ * 1. It divides spells by their length into separate arrays in spells_divided array
+ * Assumption: No 1-letter spells - could be easily fixed but code is much clearer without it.
+ *
+ * 2. It verifies if spell is correct - If it doesn't contain more then one FE or doesn't end with AI, 0 is returned.
+ * It also removes letters not being part of the spell.
+ *
+ * 3. It uses a dynamic programming based approach to solve the problem:
+ * 3.1. It calls a damagesover function on a last letter of the spell
+ * 3.2. The function checks each possible length of a spell, if it finds an existing spell of this length it calculates the maximum damage by adding
+ * the spell damage and damage of the previous part of the spell (recursion call) It also includes a -1 penalty for a single stray letter.
+ * 3.3. It returns a maximum of damages of every possible spell length.
+ * 3.4. A gain table (maximum damage from letters 0 to position in the table) is used to limit the recursion stack.
+ *
+ * 4. Result is compared to 0 and maximum is returned.
  */
-var exports = {};
-var spells2 = [{s: 'fe',d:1},{s:'je',d:2},{s:'ne',d:2},{s:'ai',d:2}];
-var spells3 = [{s:'jee',d:3},{s:'dai',d:5}];
+
+var spells = [{s: 'fe',d:1},{s:'je',d:2},{s:'ne',d:2},{s:'ai',d:2},{s:'jee',d:3},{s:'dai',d:5}];
 
 exports.damage = function(spellString) {
+
+    //reads spells and categorises them by value
+    var spells_divided = [];
+    for(var i=0;i<spells.length;i++)
+    {
+        if(!(spells_divided[spells[i].s.length-1] instanceof Array)) spells_divided[spells[i].s.length-1]=[];
+        spells_divided[spells[i].s.length-1].push(spells[i]);
+    }
+
     //Verifies if string starts with fe, fe doesn't repeat, trims letters preceding
     var beginning_pos = spellString.indexOf('fe');
     if(beginning_pos!==spellString.lastIndexOf('fe') || beginning_pos===-1) return 0;
@@ -14,59 +38,35 @@ exports.damage = function(spellString) {
     //trims letters after last ai
     spellString=spellString.substring(0,spellString.lastIndexOf('ai')+2);
 
-    var maximum_gain_ref = { v: [] };
+    //dynamic programming gain table
+    var maximum_gain=[];
 
-    return Math.max(0,damagesolver(spellString,maximum_gain_ref,spellString.length-1,spellString.length-1));
+    //comparision to avoid negative damage
+    return Math.max(0,damagesolver(spells_divided,spellString,maximum_gain,spellString.length-1,spellString.length-1));
 }
 
-function damagesolver(spellString,maximum_gain_ref,pos,length)
+function damagesolver(spells_divided,spellString,maximum_gain,pos,length)
 {
     if(pos<0)   return 0;   //no letters left, damage is 0
-    if(pos in maximum_gain_ref.v) return maximum_gain_ref.v[pos];
-    var max_damage=-Infinity;
-  //  var max_damage_letter_count=NaN;
-    switch(pos){
-        default:
-            //there are 3 or more letters, try all combinations
-            for(var i=0;i<spells3.length;i++)
-            {
-                if(spellString.substring(pos-2,pos+1)===spells3[i].s)
-                {
-                    max_damage=spells3[i].d+damagesolver(spellString,maximum_gain_ref,pos-3,length);
-                  //  max_damage_letter_count=3;
-                    break;
-                }
-            }
-        case 1:
-            //there are two letters, save damage best two letter
-            for(var i=0;i<spells2.length;i++)
-            {
-                if(spellString.substring(pos-1,pos+1)===spells2[i].s && spells2[i].d+damagesolver(spellString,maximum_gain_ref,pos-2,length)>max_damage)
-                {
-                    max_damage=spells2[i].d+damagesolver(spellString,maximum_gain_ref,pos-2,length);    //repeat call doesn't matter as result stored in array
-                 //   max_damage_letter_count=2;
-                    break;
-                }
-            }
 
-        case 0:
-        //there is one more letter, damage is -1 as there are no 1-letter spells
-            if(-1+damagesolver(spellString,maximum_gain_ref,pos-1,length)>max_damage) {
-                max_damage = -1 + damagesolver(spellString, maximum_gain_ref, pos - 1, length);
-            }
+    if(pos in maximum_gain) return maximum_gain[pos]; //value already has been calculated
+
+    var max_damage=-Infinity; //to find maximum
+
+   for(var spl_len=pos+1;spl_len>1;spl_len--) { //itterate over every possible spell length not larger then position
+       if(!(spells_divided[spl_len-1] instanceof Array)) continue;
+       for(var i=0;i<spells_divided[spl_len-1].length;i++)
+       {
+           if(spellString.substring(pos-spl_len+1,pos+1)===spells_divided[spl_len-1][i].s)
+           {
+               max_damage=spells_divided[spl_len-1][i].d+damagesolver(spells_divided,spellString,maximum_gain,pos-spl_len,length);
+               break;
+           }
+       }
+   }
+    if(-1+damagesolver(spells_divided,spellString,maximum_gain,pos-1,length)>max_damage) {
+        max_damage = -1 + damagesolver(spells_divided,spellString, maximum_gain, pos - 1, length);
     }
-    maximum_gain_ref.v[pos] = max_damage;
+    maximum_gain[pos] = max_damage;
     return max_damage;
-
 }
-
-
-alert(
-    String(exports.damage('xxxfeeaixxx') == 2)+
-    String(exports.damage('feeai') == 2)+
-    String(exports.damage('feaineain') == 7)+
-    String(exports.damage('jee') == 0) +
-    String(exports.damage('fefefefefeaiaiaiaiai') == 0)+
-    String(exports.damage('fdafafeajain') == 1) +
-    String(exports.damage('fexxxxxxxxxxai') == 0)
-);
